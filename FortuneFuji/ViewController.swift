17d8d06d0759176f3214cd71a8ea3e2a4f7223cd
Wasith Theerapattrathamrong <phai@live.com>
2018-03-08 23:38:45 +0900

    Initial Commit

---
+++ FortuneFuji/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FortuneFuji
+//
+//  Created by Wasith Theerapattrathamrong on 8/3/2561 BE.
+//  Copyright © 2561 Wasith Theerapattrathamrong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
